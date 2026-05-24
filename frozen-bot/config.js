61312350e53c701dcb14202ccca476341ef4b593
zensttt <zunsttt@gmail.com>
2026-05-24 12:54:20 +0200

    Update config.js

--- frozen-bot/config.js
+++ frozen-bot/config.js
@@ -24,34 +24,6 @@ module.exports = {
     "• Ton leaderboard à jour"
   ].join("\n"),
 
-  packs: {
-    son: [
-      // { name: "Pack son 1", url: "https://ton-lien.com" }
-    ],
-
-    graphique: {
-      fps: [
-        { name: "v3", url: "À_REMPLACER_PAR_UN_LIEN" },
-        { name: "opti_Pack2", url: "À_REMPLACER_PAR_UN_LIEN" },
-        { name: "Halo FPS UGL", url: "À_REMPLACER_PAR_UN_LIEN" },
-        { name: "fps_5ym", url: "À_REMPLACER_PAR_UN_LIEN" },
-        { name: "Opi citizen", url: "À_REMPLACER_PAR_UN_LIEN" },
-        { name: "All day", url: "À_REMPLACER_PAR_UN_LIEN" },
-        { name: "Blue", url: "À_REMPLACER_PAR_UN_LIEN" },
-        { name: "citizen", url: "À_REMPLACER_PAR_UN_LIEN" },
-        { name: "xv updated", url: "À_REMPLACER_PAR_UN_LIEN" },
-        { name: "red opti", url: "À_REMPLACER_PAR_UN_LIEN" },
-        { name: "Aqua", url: "À_REMPLACER_PAR_UN_LIEN" },
-        { name: "red fps by mystics", url: "À_REMPLACER_PAR_UN_LIEN" },
-        { name: "16", url: "À_REMPLACER_PAR_UN_LIEN" }
-      ],
-      cvc: [],
-      opti: [],
-      troll: [],
-      reshade: []
-    }
-  },
-
   security: {
     maxMessages: 6,
     intervalMs: 5000,
