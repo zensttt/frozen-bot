096dd17217e76ee7ea0db4c236b03bb1e46d84a0
zensttt <zunsttt@gmail.com>
2026-06-12 20:19:49 +0200

    Update config.js

--- frozen-bot/config.js
+++ frozen-bot/config.js
@@ -11,7 +11,7 @@ module.exports = {
   staffRoles: {
     fondateur: "𝑭𝒐𝒏𝒅𝒂𝒕𝒆𝒖𝒓",
     miniFondateur: "👑𝑴𝒊𝒏𝒊 𝑭𝒐𝒏𝒅𝒂𝒕𝒆𝒖𝒓 👑",
-    gerantRecruteur: "Gérant recruteur"
+    gerantRecruteur: "const ticketStaffRole =
   },
 
   recruitmentMessage:
